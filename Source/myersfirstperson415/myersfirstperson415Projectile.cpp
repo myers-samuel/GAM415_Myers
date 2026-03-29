44c564599b0691ae068789d8efdb846abfb4206b
Samuel Myers <samuel.myers2@snhu.edu>
2026-03-29 19:54:54 -0400

    Procedural Terrain

--- Source/myersfirstperson415/myersfirstperson415Projectile.cpp
+++ Source/myersfirstperson415/myersfirstperson415Projectile.cpp
@@ -8,6 +8,7 @@
 #include "Kismet/GameplayStatics.h"
 #include "NiagaraFunctionLibrary.h"
 #include "NiagaraComponent.h"
+#include "PerlinProcTerrain.h"
 
 
 Amyersfirstperson415Projectile::Amyersfirstperson415Projectile() 
@@ -82,5 +83,13 @@ void Amyersfirstperson415Projectile::OnHit(UPrimitiveComponent* HitComp, AActor*
 
 		MatInstance->SetVectorParameterValue("Color", randColor);
 		MatInstance->SetScalarParameterValue("Frame", frameNum);
+
+		APerlinProcTerrain* procTerrain = Cast<APerlinProcTerrain>(OtherActor);
+
+		if (procTerrain)
+		{
+			procTerrain->AlterMesh(Hit.ImpactPoint);
+		}
+
 	}
 }
